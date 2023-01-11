37621824428fbee8b76abb0a739f18d9b49bca54
oluciams <oluciams84@gmail.com>
2023-01-11 18:05:09 -0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./styles.css";
+
+function App() {
+  return (
+    <div className="App">
+      <section className="home">
+        <p className="timer">15:00</p>
+        <p>START</p>
+      </section>
+    </div>
+  );
+}
+
+export default App;
